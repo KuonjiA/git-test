cbb2b37a396dfdce330c4d988361a1475e67ae9e
arcuied <arcuied@qq.com>
2022-11-04 14:50:25 +0800

    second commit

--- src/main/java/me/arcuied/git/GitTest.java
+++ src/main/java/me/arcuied/git/GitTest.java
@@ -7,5 +7,6 @@ package me.arcuied.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello arcuied");
     }
 }
