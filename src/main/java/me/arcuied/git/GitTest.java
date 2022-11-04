b6ea0b6b727654125dfb0a0809ed8688955fedac
arcuied <arcuied@qq.com>
2022-11-04 14:59:28 +0800

    hot-fix commit1

--- src/main/java/me/arcuied/git/GitTest.java
+++ src/main/java/me/arcuied/git/GitTest.java
@@ -8,5 +8,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello arcuied");
+        System.out.println("hello ciel");
+        System.out.println("hello akiha");
     }
 }
