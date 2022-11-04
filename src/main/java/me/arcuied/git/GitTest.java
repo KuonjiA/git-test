8bcb2706c31d89f46f4039709a2566ce7806748f
arcuied <arcuied@qq.com>
2022-11-04 14:48:14 +0800

    first commit

---
+++ src/main/java/me/arcuied/git/GitTest.java
@@ -0,0 +1,11 @@
+package me.arcuied.git;
+
+/**
+ * @author arcuied
+ * @create 2022-11-04 14:43
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
